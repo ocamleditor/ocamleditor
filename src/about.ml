8a227a33fc2aeaf068fae55e083e3ea739366dea
Francesco Tovagliari <ftovagliari@gmail.com>
2014-11-24 18:27:32 +0100

    Version 1.13.2

--- src/about.ml
+++ src/about.ml
@@ -22,7 +22,7 @@
 
 
 let program_name = "OCamlEditor"
-let version = "1.13.1"
+let version = "1.13.2"
 let copyright = "Copyright © 2010 - 2014 Francesco Tovagliari"
 let website_address = "ocamleditor.forge.ocamlcore.org"
 let website = "http://" ^ website_address ^ "/"
