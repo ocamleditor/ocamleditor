80b74274113e0c5850813aed1017c23b322c2f80
Francesco Tovagliari <ftovagliari@gmail.com>
2023-08-06 18:51:54 +0200

    Copy the icons in two different paths for light or dark theme and related Icons module.

--- tools/mkicons.ml
+++ tools/mkicons.ml
@@ -22,29 +22,71 @@
 
 
 #cd "src"
-#use "../tools/scripting.ml"
+    #use "../tools/scripting.ml"
 
 open Printf
 
+let generate ochan path =
+  let icons = ".." // "icons" // path in
+  let files =
+    icons
+    |> Sys.readdir
+    |> Array.to_list
+    |> List.sort Stdlib.compare
+    |> List.filter (fun x -> Filename.check_suffix x ".png")
+  in
+  try
+    fprintf ochan "module %s = struct\n" (String.capitalize_ascii path);
+    let icon_names =
+      files |>
+      List.map begin fun file ->
+        let new_name = Str.global_replace (Str.regexp "-") "_" file in
+        Sys.rename (icons // file) (icons // new_name);
+        let icon_name = Filename.basename (Filename.chop_extension new_name) in
+        fprintf ochan "  let %s = GdkPixbuf.from_file (App_config.application_icons // \"%s\" // \"%s\")\n"
+          icon_name path new_name;
+        icon_name
+      end;
+    in
+    (*fprintf ochan "  let all_icons = [\n    %s\n  ]\n"
+      begin
+        icon_names
+        |> List.rev
+        |> List.fold_left (fun acc n ->
+            match acc with
+            | [] -> [[n]]
+            | hd :: tl when String.concat "  " hd |> String.length < 55 -> (n :: hd) :: tl
+            | hd :: tl -> [n] :: acc
+          ) []
+        |> List.map (String.concat "; ")
+        |> String.concat ";\n    "
+      end;*)
+    fprintf ochan "end\n\n";
+    icon_names
+  with _ ->
+    [];;
+
 let mkicons () =
   if not (Sys.file_exists "icons") then (mkdir "icons");
-  let icons = ".." // "icons" in
-  let files = Array.to_list (Sys.readdir icons) in
-  let files = List.filter (fun x -> Filename.check_suffix x ".png") files in
-  let cat = if Sys.os_type = "Win32" then "TYPE" else "cat" in
-  sys_command [cat; (".."//"header"); ">"; ("icons"//"icons.ml")];
   let filename = "icons/icons.ml" in
-  let ochan = open_out_gen [Open_append; Open_binary] 0o644 filename in
+  let ochan = open_out_gen [Open_creat; Open_trunc; Open_append; Open_binary] 0o644 filename in
   try
-    fprintf ochan "let (//) = Filename.concat\n\nlet create pixbuf = GMisc.image ~pixbuf ()\n\n";
-    List.iter begin fun file ->
-      let new_name = Str.global_replace (Str.regexp "-") "_" file in
-      Sys.rename (icons // file) (icons // new_name);
-      let icon_name = Filename.basename (Filename.chop_extension new_name) in
-      fprintf ochan "let %s = GdkPixbuf.from_file (App_config.application_icons // \"%s\")\n" icon_name new_name
-    end files;
+    fprintf ochan "type themed_icon = { light : GdkPixbuf.pixbuf; dark : GdkPixbuf.pixbuf }\n\n";
+    fprintf ochan "let (//) = Filename.concat\n\n";
+    fprintf ochan "let create pixbuf = GMisc.image ~pixbuf ()\n\n";
+    let light_names = generate ochan "light" in
+    let dark_names = generate ochan "dark" in
+    assert (List.length light_names = List.length dark_names);
+    light_names
+    |> List.iter begin fun name ->
+      fprintf ochan "let %s = Light.%s, Dark.%s\n" name name name;
+    end;
     close_out_noerr ochan;
-  with _ -> close_out_noerr ochan;;
+  with ex ->
+    begin
+      Printf.eprintf "File \"mkicons.ml\": %s\n%s\n%!" (Printexc.to_string ex) (Printexc.get_backtrace());
+      close_out_noerr ochan
+    end
 
 let _ = main ~default_target:mkicons ~options:[] ()
 
