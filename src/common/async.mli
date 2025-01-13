a1abfdb9e01feb10f196bdcf67c95009dddf6b8c
Francesco Tovagliari <ftovagliari@gmail.com>
2025-01-13 08:57:01 +0100

    Add Async, a lightweight framework for handling tasks and computations with basic support for asynchronous execution and synchronization.

---
+++ src/common/async.mli
@@ -0,0 +1,14 @@
+type 'a expr
+type 'a task
+(*val return : ?name:string -> (unit -> 'a) -> 'a expr*)
+val create : ?name:string -> (unit -> 'a) -> 'a expr
+(*val bind : ('a -> 'b expr) -> 'a expr -> 'b expr
+  val ( >>= ) : 'a expr -> ('a -> 'b expr) -> 'b expr*)
+val map : ?name:string -> ('a -> 'b) -> 'a expr -> 'b expr
+val continue_with : ?name:string -> ('a -> 'b) -> 'a expr -> 'b expr
+(*val ( let* ) : 'a expr -> ('a -> 'b expr) -> 'b expr*)
+val ( let+ ) : 'a expr -> ('a -> 'b) -> 'b expr
+val start_as_task : 'a expr -> 'a task
+val start : 'a expr -> unit
+val await : 'a task -> 'a
+val run_synchronously : 'a expr -> 'a
